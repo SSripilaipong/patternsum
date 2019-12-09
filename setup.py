2275c171548c62c2de6746ccd76b119e3edb2098
SSripilaipong <vappakarat@gmail.com>
2019-12-09 23:20:09 +0700

    update version since I fucked up with the repository

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='patternsum',
-    version='1.0',
+    version='1.1',
     packages=setuptools.find_packages(),
     url='https://github.com/SSripilaipong/string-pattern-summ',
     author='SSripilaipong',
