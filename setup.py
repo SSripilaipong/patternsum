9f6863027280fedb2d5eaca978adb0b070c9c941
SSripilaipong <vappakarat@gmail.com>
2019-12-09 23:24:54 +0700

    change url

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setuptools.setup(
     name='patternsum',
     version='1.1',
     packages=setuptools.find_packages(),
-    url='https://github.com/SSripilaipong/string-pattern-summ',
+    url='https://github.com/SSripilaipong/patternsum',
     author='SSripilaipong',
     author_email='SHSnail@mail.com',
     description='An algorithm to summarize possible patterns covered in a list of strings using Genetic Algorithm.',
